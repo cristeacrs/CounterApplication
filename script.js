f22de900b18c4333b214162f230ca97489b3da22
cristeacrs <cristea.cristi@yahoo.com>
2023-01-16 23:16:59 +0200

    [FEAT]: update function add/subtract

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-let add = document.querySelector("#add");
-
 add.addEventListener("click", function () {
   let output = document.querySelector("#output");
-  let result = output.innerText + 1;
+  let result = Number(output.innerText) + 1;
+
+  if (result > 10) {
+    result = 0;
+  }
+
+  output.innerText = result;
+});
+
+subtract.addEventListener("click", function () {
+  let output = document.querySelector("#output");
+  let result = Number(output.innerText) - 1;
+
+  if (result < 0) {
+    result = 0;
+  }
 
   output.innerText = result;
 });
